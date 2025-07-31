f44add8fb721b6f681641bcf8eea15678d82c9b8
forampaneliya <forampaneliya1204@gmail.com>
2025-07-31 15:17:04 +0530

    create sub page of service

--- Pathsy-frontend/src/AllRoutes.jsx
+++ Pathsy-frontend/src/AllRoutes.jsx
@@ -8,6 +8,8 @@ import Contact from './pages/Contact'
 import HelpAndSupport from './pages/HelpAndSupport'
 import WhoWeAre from './pages/WhoWeAre'
 import WhyChoose from './pages/WhyChoose'
+import TechEnabledWarehousing from './pages/Services/TechEnabledWarehousing'
+import ExhibitionLogisticsDesign from './pages/Services/ExhibitionLogisticsDesign'
 
 function AllRoutes() {
     return (
@@ -20,6 +22,8 @@ function AllRoutes() {
                 <Route path="/whyChoose" element={<WhyChoose/>}/>
                 <Route path="/careers" element={<Carrers />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="/services/techEnabledWarehousing" element={<TechEnabledWarehousing/>}/>
+                <Route path="serices/exhibitionLogisticsDesign" element={<ExhibitionLogisticsDesign/>}/>
                 {/* <Route path="/helpAndSupport" element={<HelpAndSupport/>}/> */}
 
 
